3e765bc43a7d7ef6817905a96283eea8faa5c49c
Geli Yang <gyang5@villanova.edu>
2022-06-30 11:48:36 -0400

    Capture file uploaded in uploads folder
    Test upload function locally

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express")
+const multer = require("multer")
 const app = express()
+
+const upload = multer({ dest: "uploads" })
 
 app.set("view engine", "ejs")
 
 app.get("/", (req,res)=>{
     res.render("index")
 
+})
+
+
+app.post("/upload", upload.single("file"),(req,res)=>{
+    res.send("hi")
 })
 
 app.listen(3000)
